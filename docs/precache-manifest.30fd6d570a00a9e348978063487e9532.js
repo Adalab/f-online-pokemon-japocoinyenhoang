d3a367664e1b10202e203d27dd25bf64d1cb44d8
yen <japocoin@gmail.com>
2019-03-19 00:54:35 +0100

    create doc file

---
+++ docs/precache-manifest.30fd6d570a00a9e348978063487e9532.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "377f8ba2c1c916defda4",
+    "url": "./static/js/main.c7211a40.chunk.js"
+  },
+  {
+    "revision": "bb925ff48b846a8400a2",
+    "url": "./static/js/2.4bcdc629.chunk.js"
+  },
+  {
+    "revision": "377f8ba2c1c916defda4",
+    "url": "./static/css/main.1af5d6db.chunk.css"
+  },
+  {
+    "revision": "70b0c6ccd90eb4e017ae9fe1116b7f2c",
+    "url": "./index.html"
+  }
+];
